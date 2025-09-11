8a7c018b5eb772dcd69ec1fa55268821eff42c69
LuckylisaBemeye <luckylisa.bemeye@strathmore.edu>
2025-09-11 17:27:58 +0300

    Removed unnecessary things

--- Forms/forms.php
+++ Forms/forms.php
@@ -2,7 +2,9 @@
 class forms {
     public function signup() {
 ?>
-    <form method="post" action="../Plugins/phpmailer/main.php">
+    <h2 style="text-align:center; color:#2c3e50;">Welcome to My Task App!</h2>
+    <p style="text-align:center; font-size:1.1em; color:#34495e;">Ready to organize your life and boost your productivity? Register now and join a community of achievers! Your journey to getting things done starts here.</p>
+    <form method="post" action="../Plugins/phpmailer/main.php" style="max-width:400px; margin:auto;">
         <label for="username">Username:</label>
         <input type="text" id="username" name="username" required>
         <br><br>
@@ -12,7 +14,7 @@ class forms {
         <label for="password">Password:</label>
         <input type="password" id="password" name="password" required>
         <br><br>
-        <input type="submit" value="Sign Up"> <a href="signin.php">Already have an account? Log in</a>
+        <input type="submit" value="Sign Up">
     </form>
 <?php
     }
--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
-// Include the ClassAutoLoad Method
+
 require_once 'ClassAutoLoad.php';
-$layout->header($conf);
+
 print $hello->today();
 $form->signup();
-$layout->footer($conf);
