ccf012b4b7b6a94e1a4ade62d2ecc25ba42c9109
LuckylisaBemeye <luckylisa.bemeye@strathmore.edu>
2025-09-11 17:08:52 +0300

    Second commit

--- conf.php
+++ conf.php
@@ -1,15 +1,15 @@
 <?php
 // Site Information
-$conf['site_name'] = 'ICS C Community';
+$conf['site_name'] = '168145 Task App';
 $conf['site_url'] = 'http://localhost';
-$conf['admin_email'] = 'admin@icsccommunity.com';
+$conf['admin_email'] = 'admin@168145taskapp.com';
 
 // Database Configuration
 $conf['db_type'] = 'pdo';
 $conf['db_host'] = 'localhost';
 $conf['db_user'] = 'root';
 $conf['db_pass'] = '';
-$conf['db_name'] = 'nol';
+$conf['db_name'] = '168145_task_app';
 
 // Site Language
 $conf['site_lang'] = 'en';
